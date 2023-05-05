4e1a2f294d93a70c93bd29bb31266dac443e104e
Manu <manu.web3.0@gmail.com>
2023-05-05 17:34:25 -0600

    Added custom 404.js

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function PageNotFound () {
+    return <h1>Han bai...kiddar nu mooh chakkeya... URL Not found</h1>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
+// Static generation without external data
+// All content known at the Build time already by the page / developer.
+
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 function Home() {
+
+    const router = useRouter()
+    const handleClick = () => {
+        console.log("Placing your order...")
+        router.push("/product")
+        
+        // router.replace("/product")
+
+        // (1). "at index.js of /product, clicking on "Product#3" and then "back" button
+        // still takes to home page (pages/index.js)
+        // => Link - "replace" still runs
+
+        //(2). /product/.....whatever you may think to navigae to can be added
+        // static / dynamic
+
+        // (3). to replace the history instead of pushing the URL on the stack -> replace
+    }
+
     return (
         <>
             <h1>Hello Duniya!!</h1>
@@ -12,6 +34,11 @@ function Home() {
             <Link href="/product" legacyBehavior>
                 <a>To Product</a>
             </Link>
+            <br></br>
+            <br></br>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
         </>
     )
 }
